3677c856f1b0d072c215e8b077ecc5f4cf2760ee
Tony Messias <tonyzrp@gmail.com>
2019-12-02 20:56:37 -0300

    Calculate require fuel from input file

--- src/01-moduleFuel.test.js
+++ src/01-moduleFuel.test.js
@@ -1,4 +1,5 @@
 const calculateFuel = require('./01-moduleFuel');
+const fs = require('fs');
 
 describe('FuelCalculator', () => {
     test('calculates required fuel of modules', () => {
@@ -6,5 +7,18 @@ describe('FuelCalculator', () => {
         expect(calculateFuel(14)).toBe(2);
         expect(calculateFuel(1969)).toBe(654);
         expect(calculateFuel(100756)).toBe(33583);
+    });
+
+    test('calculates from file inputs', (done) => {
+        fs.readFile(__dirname + '/inputs/fuel-input.txt', (_err, data) => {
+            const itemsMass = data.toString().split('\n');
+            const requiredFuel = itemsMass.map(i => calculateFuel(i));
+            let sumOfRequireFuel = 0;
+            requiredFuel.forEach((s) => {
+                sumOfRequireFuel += s;
+            });
+            expect(sumOfRequireFuel).toBe(3427945);
+            done();
+        });
     });
 });
