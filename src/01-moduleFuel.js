f14c5ff3dfdb353514b63494cbf604c6fa455720
Tony Messias <tonyzrp@gmail.com>
2019-12-02 21:48:53 -0300

    Calculate deep fuel from file

--- src/01-moduleFuel.js
+++ src/01-moduleFuel.js
@@ -1,3 +1,16 @@
 const calculateFuel = (mass) => Math.floor(mass / 3) - 2;
 
-module.exports = calculateFuel;
+const deepFuel = (mass) => {
+    const fuel = calculateFuel(mass);
+
+    if (fuel <= 0) {
+        return 0;
+    }
+
+    return fuel + deepFuel(fuel);
+};
+
+module.exports = {
+    calculateFuel,
+    deepFuel,
+};
--- src/01-moduleFuel.test.js
+++ src/01-moduleFuel.test.js
@@ -1,4 +1,4 @@
-const calculateFuel = require('./01-moduleFuel');
+const { calculateFuel, deepFuel } = require('./01-moduleFuel');
 const fs = require('fs');
 
 describe('FuelCalculator', () => {
@@ -17,6 +17,24 @@ describe('FuelCalculator', () => {
                 .reduce((acc, mass) => acc + calculateFuel(parseInt(mass)), 0);
 
             expect(sumOfRequireFuel).toBe(3427947);
+            done();
+        });
+    });
+
+    test('deep fuel calculation', () => {
+        expect(deepFuel(0)).toBe(0);
+        expect(deepFuel(14)).toBe(2);
+        expect(deepFuel(1969)).toBe(966);
+        expect(deepFuel(100756)).toBe(50346);
+    });
+
+    test('deep fuel from file input', (done) => {
+        fs.readFile(__dirname + '/inputs/fuel-input.txt', (_err, data) => {
+            const sumOfRequireFuel = data.toString()
+                .trim().split('\n')
+                .reduce((acc, mass) => acc + deepFuel(parseInt(mass)), 0);
+
+            expect(sumOfRequireFuel).toBe(5139037);
             done();
         });
     });
