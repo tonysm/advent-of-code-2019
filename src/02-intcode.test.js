4c7e16a6e3c97c687524f0f46871e3355efeb218
Tony Messias <tonyzrp@gmail.com>
2019-12-03 22:31:06 -0300

    Computes intcode from file

--- src/02-intcode.test.js
+++ src/02-intcode.test.js
@@ -1,4 +1,5 @@
 const { intCodeComputer } = require('./02-intcode');
+const fs = require('fs');
 
 describe('IntCode', () => {
     test('computes intcode', () => {
@@ -6,5 +7,21 @@ describe('IntCode', () => {
         expect(intCodeComputer([2,3,0,3,99])).toEqual([2,3,0,6,99]);
         expect(intCodeComputer([2,4,4,5,99,0])).toEqual([2,4,4,5,99,9801]);
         expect(intCodeComputer([1,1,1,4,99,5,6,0,99])).toEqual([30,1,1,4,2,5,6,0,99]);
+    });
+
+    test('computes intcode from file', (done) => {
+        fs.readFile(__dirname + '/inputs/intcodes-input.txt', (_err, data) => {
+            const codes = data
+                .toString()
+                .trim()
+                .split(',')
+                .map(i => parseInt(i));
+
+            codes[1] = 12;
+            codes[2] = 2;
+
+            expect(intCodeComputer(codes)[0]).toEqual(5434663);
+            done();
+        });
     });
 });
